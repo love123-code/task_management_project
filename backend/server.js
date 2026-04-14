ae32cc2d02276e82808ccbfecc7bcdca5686b50e
love123-code <lovesharma819281@gmail.com>
2026-04-14 23:27:41 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,38 +2,51 @@ const express = require('express');
 const cors = require('cors');
 const http = require('http');
 const { Server } = require('socket.io');
+
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 const boardRoutes = require('./routes/boardRoutes');
-app.get("/", (req, res) => {
-  res.send("Backend live hai 🚀");
-});
 const initializeSocket = require('./socket');
+
 
 const app = express();
 const server = http.createServer(app);
+
+
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    origin: '*', // temporary (baad me frontend URL dal dena)
     methods: ['GET', 'POST', 'PUT', 'DELETE']
   }
 });
 
+
 connectDB();
+
 
 app.use(cors());
 app.use(express.json());
 
+
 app.use('/api/auth', authRoutes);
 app.use('/api/boards', boardRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Backend live hai 🚀");
+});
+
 
 app.get('/api/health', (req, res) => {
   res.json({ status: 'ok' });
 });
 
+
 initializeSocket(io);
 
+
 const PORT = process.env.PORT || 5000;
+
+
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
